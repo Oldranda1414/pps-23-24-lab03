dfd09b8863f8fe9770452428c0313097ee6edc91
Oldranda <leonardorandacio@gmail.com>
2024-03-18 14:53:57 +0100

    completed ex 3

---
+++ src/main/scala/u03/Courses.scala
@@ -0,0 +1,14 @@
+package u03
+
+import u03.Sequences.Sequence
+import u03.Sequences.Sequence.*
+import u02.AlgebraicDataTypes.Person
+import u02.AlgebraicDataTypes.Person.*
+
+object Courses:
+
+    def courses(l: Sequence[Person]): Sequence[String] = 
+        flatMap(l)(_ match
+        case Teacher(_, course) => Cons(course, Nil())
+        case _ => Nil()
+      )
