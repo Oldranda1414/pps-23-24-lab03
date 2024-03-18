df20509f435cfbb2dbf28645dfca2905adb8e069
Oldranda <leonardorandacio@gmail.com>
2024-03-18 19:01:21 +0100

    completed ex 7

--- src/main/scala/u03/StreamFunctions.scala
+++ src/main/scala/u03/StreamFunctions.scala
@@ -3,9 +3,13 @@ package u03
 import extensionmethods.Streams.Stream
 import extensionmethods.Streams.Stream.*
 
-object TakeWhile:
+object StreamFunctions:
     def takeWhile[A](s: Stream[A])(pred: A => Boolean): Stream[A] = s match
         case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail())(pred))
         case _ => Empty()
     
         
+    def fill[A](n: Int)(item: A): Stream[A] = n match
+        case n if n > 0 => Cons(() => item, () => fill(n - 1)(item))
+        case _ => Empty()
+    
