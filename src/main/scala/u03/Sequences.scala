58f9799f0d871d494ad86bec82d929c9664edbf2
Oldranda <leonardorandacio@gmail.com>
2024-03-18 11:34:52 +0100

    completed part 1 d

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -16,14 +16,14 @@ object Sequences: // Essentially, generic linkedlists
       case Cons(h, t) => h + sum(t)
       case _          => 0
 
-    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil()      => Nil()
+    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = 
+      flatMap(l)(v => Cons(mapper(v), Nil()))
 
-    def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = l1 match
-      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
-      case Cons(_, t)            => filter(t)(pred)
-      case Nil()                 => Nil()
+    def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = 
+      flatMap(l1)(v => pred(v) match
+        case true => Cons(v, Nil())
+        case _ => Nil()
+      )
 
     // Lab 03
     def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = first match
@@ -31,7 +31,6 @@ object Sequences: // Essentially, generic linkedlists
         case Cons(h2, t2) => Cons((h1, h2), zip(t1, t2))
         case _ => Nil()
       case _ => Nil()
-      
 
     def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
       case Cons(head, tail) if n > 0=> Cons(head, take(tail)(n-1))
